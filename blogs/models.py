e60b20c66ee1feae1f68186878460b3b4dca838b
Hector Vazquez <hvazquezglez@gmail.com>
2019-05-23 13:12:34 +0100

    modelo inicial post

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    name = models.CharField(max_lenght=150)
+    description = models.TextField()
+    content = models.TextField()
+    url = models.URLField(null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+    publication_date = models.DateTimeField(default=datetime.datetime.now)
+    categories = models.ManyToManyField(Category)
+
